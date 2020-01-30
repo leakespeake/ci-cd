1c9ea390f72787dfd1c69acaa5db97015f2c9c15
unknown <45919758+leakespeake@users.noreply.github.com>
2020-01-30 15:07:09 +0000

    amended path to s3 state file

--- Chapter3/stage/services/webserver-cluster/backend.tf
+++ Chapter3/stage/services/webserver-cluster/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket      = "leakespeake-terraform-state-files"
-        key         = "global/s3/terraform.tfstate"
+        key         = "global/s3/Chapter3/stage/services/webserver-cluster/terraform.tfstate"
         region      = "us-east-2"
 
         dynamodb_table  = "leakespeake-terraform-state-lock"
